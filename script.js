fc22a71bf72474d6185119d4c8c110021cd0f467
Pratik Singh <50449708+pratiksingh5@users.noreply.github.com>
2021-01-05 17:15:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,53 +2,81 @@ let canvas = document.querySelector("#canvas");
 let time = document.querySelector("#time");
 let guess = document.querySelector("#guess");
 let score = document.querySelector("#score");
+let btn = document.querySelector('.btn');
+let final = document.querySelector('#final')
+let point = document.querySelector('#points');
+let loader = document.querySelector('#loader');
+
+let animate = gsap.timeline();
+
+animate.to('.lds-grid',1.5,{
+  delay:1.1,
+  opacity:0
+},)
+
+animate.to('#one',1,{
+  x:'-100%',
+ ease: "power4.out"
+})
+animate.to('#two',1,{
+  x:'100%',
+ ease: "power4.out"
+},"-=1")
+
+setTimeout(function(){
+ loader.style.display= "none";
+},4000)
 
 
-
-
-function makeBubbles(){
-    for(i=0;i<44;i++){
-        let randomNumber =Math.floor(Math.random()*10) ;
-        console.log(randomNumber);
-        canvas.innerHTML +=  `<div class="bubble">${randomNumber}</div>`;
-    }
+function makeBubbles() {
+  for (i = 0; i < 45; i++) {
+    let randomNumber = Math.floor(Math.random() * 10);
+  
+    canvas.innerHTML += `<div class="bubble">${randomNumber}</div>`;
+  }
 }
 makeBubbles();
 
-let start = 5;
+let start = 63;
 
-setInterval(timer,1000);
+setInterval(timer, 1000);
 
-function timer(){
-    if(start>0){
-        start--;
-        time.innerHTML = start;
-    }
-    else{
-       canvas.innerHTML = `<h1> Game Over </h1>`;
-       canvas.style.backgroundColor=' rgb(34, 131, 131)';
-    }
+function timer() {
+  if (start > 0) {
+    start--;
+    time.innerHTML = start;
+  } else {
+    canvas.style.display = 'none';
+    final.style.display ='block';
+    point.innerHTML= points;
+   
+    canvas.style.backgroundColor = "#fefefe";
+  }
 }
 
-function guessNumber(){
-    let randomGuess = Math.floor(Math.random()*10);
-    guess.innerHTML = randomGuess;
+function guessNumber() {
+  let randomGuess = Math.floor(Math.random() * 10);
+  guess.innerHTML = randomGuess;
 }
 
 guessNumber();
 
-let points=0;
+let points = 0;
 
-canvas.addEventListener('click',function(e){
-console.log(e);
-let num = e.target.textContent;
-console.log(num);
-if(num== guess.textContent){
-    points = points + 10;
-    console.log('clicked right');
+canvas.addEventListener("click", function (e) {
+  console.log(e);
+  let num = e.target.textContent;
+ 
+  if (num == guess.textContent) {
+    points = points + 1;
+    
     score.innerHTML = points;
-    canvas.innerHTML= '';
+    canvas.innerHTML = "";
     makeBubbles();
     guessNumber();
-}
+  }
+});
+
+btn.addEventListener('click', function(){
+  location.reload();
 })
